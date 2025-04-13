8ba5ce65a12dc6193b69c06d7def3777e3043d36
kanaru-ssk <kanaru.ssk@gmail.com>
2025-04-13 13:53:43 +0900

    レイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="flex h-full flex-col items-center justify-center space-y-8">
 			<div>
+				<p>このサイトの説明文を入れる。</p>
 				<p>このサイトの説明文を入れる。</p>
 			</div>
 			<Button size="lg" className="w-28">
