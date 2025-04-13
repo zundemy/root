4cb8533850810f50f378ae81f38f9ea61cdea93b
kanaru-ssk <kanaru.ssk@gmail.com>
2025-04-13 23:43:42 +0900

    style調整

--- src/app/getting-started/page.tsx
+++ src/app/getting-started/page.tsx
@@ -1,51 +1,5 @@
 import { Code } from "@/components/code";
 
-const exampleSnippets = [
-	{
-		title: "With line numbers",
-		snippet: `
-  \`\`\`ts {4} showLineNumbers
-  const add = (a: number, b: number): number => a + b;
-  \`\`\`
-  `,
-	},
-	{
-		title: "With title and line numbers",
-		snippet: `
-  \`\`\`rust title="fib.erl" showLineNumbers
-  -spec is_palindrome(X :: integer()) -> boolean().
-  -import(lists, [reverse/1]).
-  
-  is_palindrome(X) ->
-	  S = integer_to_list(X),
-	  R = reverse(S),
-	  S =:= R.
-  \`\`\`
-  `,
-	},
-	{
-		title: "With title, line numbers, and character highlighting",
-		snippet: `
-  \`\`\`elixir title="fib.ex" showLineNumbers /palindrome/#v
-  defmodule Solution do
-	@spec is_palindrome(x :: integer) :: boolean
-	def is_palindrome(x) when x < 0, do: false
-	def is_palindrome(x), do: do_is_palindrome(x, get_base_10(x, 1))
-  
-	defp do_is_palindrome(x, b10) when b10 > 1,
-	  do: get_first_digit(x, b10) == rem(x, 10) and do_is_palindrome(div(x, 10), div(b10, 100))
-  
-	defp do_is_palindrome(_, _), do: true
-  
-	defp get_first_digit(n, b10), do: div(n, b10) |> rem(10)
-  
-	defp get_base_10(n, b10) when n >= b10, do: get_base_10(n, b10 * 10)
-	defp get_base_10(n, b10), do: div(b10, 10)
-  end
-  \`\`\``,
-	},
-];
-
 export default function Page() {
 	return (
 		<article>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Page() {
 	return (
 		<div className="flex h-full flex-col items-center justify-center space-y-8">
 			<div className="text-center leading-4">
-				<p>宮城大学の1年生必修科目</p>
 				<p>コンピューターリテラシー(2025)</p>
 				<p>第12回 ~ 第14回の授業資料です。</p>
 				<p>生成AIを使用してwebサイトを作成してみましょう。</p>
